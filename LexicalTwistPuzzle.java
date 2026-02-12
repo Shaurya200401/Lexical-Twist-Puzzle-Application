37b7d73616488db8a5ad4d12cc27dbdc116f925e
Shaurya200401 <awasthi.shaurya9@gmail.com>
2026-02-12 15:25:52 +0530

    [Shaurya]: UC7: Output Based on Vowel–Consonant Count

--- LexicalTwistPuzzle.java
+++ LexicalTwistPuzzle.java
@@ -39,8 +39,34 @@ public class LexicalTwistPuzzle {
             }
 
             System.out.println("Vowels: " + vowels + ", Consonants: " + consonants);
+
+            if (vowels > consonants) {
+                System.out.print("First 2 unique vowels: ");
+                printUniqueChars(combined, "AEIOU", 2);
+            } else if (consonants > vowels) {
+                System.out.print("First 2 unique consonants: ");
+                printUniqueChars(combined, "BCDFGHJKLMNPQRSTVWXYZ", 2);
+            } else {
+                System.out.println("No Lexical Twist Possible: Vowels and Consonants are equal.");
+            }
         }
 
         scanner.close();
+    }
+
+    private static void printUniqueChars(String str, String filter, int count) {
+        StringBuilder unique = new StringBuilder();
+        int found = 0;
+
+        for (char c : str.toCharArray()) {
+            if (filter.indexOf(c) != -1 && unique.indexOf(String.valueOf(c)) == -1) {
+                unique.append(c);
+                System.out.print(c + " ");
+                found++;
+                if (found == count)
+                    break;
+            }
+        }
+        System.out.println();
     }
 }
