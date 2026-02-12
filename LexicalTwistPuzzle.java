689115e846b8d46f331d59076b616d34cb81bf57
Shaurya200401 <awasthi.shaurya9@gmail.com>
2026-02-12 15:12:47 +0530

    [Shaurya]: UC2: Accept Word Inputs

--- LexicalTwistPuzzle.java
+++ LexicalTwistPuzzle.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class LexicalTwistPuzzle {
     public static void main(String[] args) {
         System.out.println("Lexical Twist Puzzle");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String word1 = scanner.nextLine();
+
+        System.out.print("Enter second word: ");
+        String word2 = scanner.nextLine();
+
+        scanner.close();
     }
 }
